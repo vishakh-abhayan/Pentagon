fd4ae7903f68834c8d6b1955f932fdd65404c674
Mishalabdullah <github.ijvob@simplelogin.co>
2023-08-19 21:54:58 +0530

    env fixed

--- src/components/MintNFT.tsx
+++ src/components/MintNFT.tsx
@@ -13,14 +13,13 @@ function MintNFT() {
       if (account && account.address) {
         console.log(account.address, "account address metamask");
         setWalletAddress(account.address); // Assuming setWalletAddress is the setter for the account state
-        console.log(config);
       }
     }
 
     fetchAccount();
   }, []);
   const { config } = usePrepareContractWrite({
-    address: "0x0BF5f3C80C2fE5575c1c2c787B352868fdf76858",
+    address: import.meta.env.VITE_CONTRACT_ADDRESS,
     abi: abi,
     functionName: "mintNFT",
     args: [walletAddress, metadataURL],
