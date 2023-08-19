0d4ff8af913d583723dcd62b4b7efe90cfa9f415
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-08-19 13:02:15 +0530

    up base modules

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,11 @@
+import Home from "../pages/home";
+
+function Routes() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default Routes;
