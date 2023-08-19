4d8b9432958aa04595291e8821acbb51b4f3cebb
Mishalabdullah <github.ijvob@simplelogin.co>
2023-08-19 16:47:03 +0530

    Added Connect Wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import React from "react";
 import "./App.css";
 import Routes from "./routes/Routes";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+// Create the configuration
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  publicClient,
+  webSocketPublicClient,
+});
 
 function App() {
-  return <Routes />;
+  return (
+    <WagmiConfig config={config}>
+      <Routes />
+    </WagmiConfig>
+  );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import ConnectWalletButton from "../components/ConnectWalletButton";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <ConnectWalletButton />
     </div>
   );
 }
