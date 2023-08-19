9ad1ee75761ac3e72fbce1bf1d4e5bac4a0c408e
Mishalabdullah <github.ijvob@simplelogin.co>
2023-08-19 18:35:10 +0530

    Minting NFT Button

---
+++ src/components/MintNFT.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { usePrepareContractWrite, useContractWrite, useAccount } from "wagmi";
+import abi from "./web3/abi.json";
+
+function MintNFT() {
+  const [walletAddress, setWalletAddress] = useState("");
+  const metadataURL =
+    "https://ipfs.io/ipfs/bafybeih3b6aqffzx6yhiv4ohlgk4cp62saszb7doyg5k6r2drzny3n5uqy/1.json";
+  const account = useAccount();
+
+  useEffect(() => {
+    async function fetchAccount() {
+      if (account && account.address) {
+        console.log(account.address, "account address metamask");
+        setWalletAddress(account.address); // Assuming setWalletAddress is the setter for the account state
+      }
+    }
+
+    fetchAccount();
+  }, []);
+
+  const { config } = usePrepareContractWrite({
+    address: "0x2b0cb13623824dec1cbfac5926229b161c947b36",
+    abi: abi,
+    functionName: "mintNFT",
+    args: [walletAddress, metadataURL],
+  });
+
+  const { write } = useContractWrite(config);
+
+  return (
+    <div>
+      <button onClick={() => write?.()}>Mint NFT</button>
+    </div>
+  );
+}
+
+export default MintNFT;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/Navbar";
 import ConnectWalletButton from "../components/ConnectWalletButton";
-
-
+import MintNFT from "../components/MintNFT";
 function Home() {
   return (
     <div>
       <Navbar />
       <ConnectWalletButton />
+      <MintNFT />
     </div>
   );
 }
